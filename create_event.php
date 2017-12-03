7c2a68af8b1ac0bb0b4b8cd2f1b25cfeb19f8070
Vladimir Beauge <VladimirBeauge@users.noreply.github.com>
2017-12-03 16:01:57 -0500

    Update create_event.php

--- create_event.php
+++ create_event.php
@@ -1,8 +1,21 @@
-// adds the number of users to the database
-( event_id, u_type, dj_pwd, event_pwd ) 
-    if(event_id.exists() && (event_id).count() > 1)
-        //generate a new event id and call self again
-    if(event_id.exists() && (event_id).count() < 1)
-        //use te entry
-  if( u_type == 'dj')
-        //give more permissions
+<?php
+ 
+require 'connection.php';
+$conn    = Connect();
+$name    = $conn->real_escape_string($_POST['e_name']);
+$email   = $conn->real_escape_string($_POST['privacy']);
+$subj    = $conn->real_escape_string($_POST['password']);
+$message = $conn->real_escape_string($_POST['dj_password']);
+$query   = "INSERT into tb_cform (e_name,privacy,password,dj_password) VALUES('" . $name . "','" . $email . "','" . $subj . "','" . $message . "')";
+$success = $conn->query($query);
+ 
+if (!$success) {
+    die("Couldn't enter data: ".$conn->error);
+ 
+}
+ 
+echo "Thank You For Contacting Us <br>";
+ 
+$conn->close();
+ 
+?>
