5226ca9f8accb289e05bc00fb3dee25f36b01a3a
Vladimir Beauge <VladimirBeauge@users.noreply.github.com>
2017-12-03 03:28:42 -0500

    Update and rename add_event.php to create_event.php

---
+++ create_event.php
@@ -0,0 +1,8 @@
+// adds the number of users to the database
+( event_id, u_type, dj_pwd, event_pwd ) 
+    if(event_id.exists() && (event_id).count() > 1)
+        //generate a new event id and call self again
+    if(event_id.exists() && (event_id).count() < 1)
+        //use te entry
+  if( u_type == 'dj')
+        //give more permissions
